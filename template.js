9466ec434ec841c42a07ef5dd7f09eba0f9fbaa0
Aaron Sullivan <aaron.sullivan@flcc.edu>
2018-03-29 21:30:47 -0400

    Added actions for buttons by defining simple functions

---
+++ template.js
@@ -0,0 +1,11 @@
+function doSomething() {
+    window.alert("test complete");
+    console.log("doSomething() was called");
+    document.querySelector("#firstPanel" ).innerHTML = "<p>First Panel Affected</p>";
+}
+
+/*
+function doSomethingElse() {
+    console.log("Test of something else complete.");
+}
+*/
