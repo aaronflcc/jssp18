eddce60e308b5911388e69469b66d59be8bd5238
Aaron Sullivan <aaron.sullivan@flcc.edu>
2018-04-03 20:36:45 -0400

    Updated color scheme and added optional 3 columns automatic layout through uncommenting

--- template.js
+++ template.js
@@ -4,8 +4,18 @@ function doSomething() {
     document.querySelector("#firstPanel" ).innerHTML = "<p>First Panel Affected</p>";
 }
 
-/*
+
 function doSomethingElse() {
+    document.querySelector("#thirdPanel").innerHTML = "<p>Look over here!</p>";
     console.log("Test of something else complete.");
 }
-*/
+
+function doAThirdThing() {
+    //create an ordered list of number values that count from 50 to 0 by 5s
+    var newStuff = "<ol>";
+    for (var count=50; count >= 0; count -= 5) {
+        newStuff += "<li>" + count + "</li>";
+    }
+    newStuff += "</ol>";
+    document.querySelector("#secondPanel").innerHTML = newStuff;
+}
